131154d408a31f78f3449d50d6e05520e7ae4080
Pietroo03 <pietro.ponte.03@gmail.com>
2025-04-01 22:25:50 +0200

    add routes

--- final-project-boolean/src/App.jsx
+++ final-project-boolean/src/App.jsx
@@ -10,6 +10,8 @@ import CreateAlbumPage from './pages/CreateAlbumPage';
 import CreateArtistPage from './pages/CreateArtistPage';
 import EditAlbumPage from './pages/EditAlbumPage';
 import EditArtistPage from './pages/EditArtistPage';
+import GenresPage from './pages/GenresPage'
+import CreateGenrePage from './pages/CreateGenre';
 
 function App() {
 
@@ -23,10 +25,12 @@ function App() {
             <Route path='/' element={<HomePage />}></Route>
             <Route path='/albums' element={<AlbumsPage />}></Route>
             <Route path='/artists' element={<ArtistsPage />}></Route>
+            <Route path='/genres' element={<GenresPage />}></Route>
             <Route path='/albums/:id' element={<SingleAlbumPage />}></Route>
             <Route path='/artists/:id' element={<SingleArtistPage />}></Route>
             <Route path='/albums/create' element={<CreateAlbumPage />}></Route>
             <Route path='/artists/create' element={<CreateArtistPage />}></Route>
+            <Route path='/genres/create' element={<CreateGenrePage />}></Route>
             <Route path='/albums/edit/:id' element={<EditAlbumPage />}></Route>
             <Route path='/artists/edit/:id' element={<EditArtistPage />}></Route>
 
